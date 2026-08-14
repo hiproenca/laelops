ce06a167dfc029120f46c1d1f580ef88d1635013
higor.proenca <higorproenca@gmail.com>
2026-08-14 00:01:02 -0300

    setup inicial: projeto Spring Boot, docker-compose com Postgres e Redis ambiente configurado. Pendente, criação de schema para o postgres no flyway

---
+++ src/main/java/com/higor/laelops/LaelopsApplication.java
@@ -0,0 +1,13 @@
+package com.higor.laelops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaelopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaelopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/higor/laelops/LaelopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.higor.laelops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaelopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
