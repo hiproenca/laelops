5309a9a4ca64c4fa21aa56aca88bbcded5a7bfd8
higor.proenca <higorproenca@gmail.com>
2026-08-15 14:50:56 -0300

    Feat: Criado usuarioService.java

--- src/main/java/com/higor/laelops/service/UsuarioService.java
+++ src/main/java/com/higor/laelops/service/UsuarioService.java
@@ -1,9 +1,24 @@
 package com.higor.laelops.service;
 
 import com.higor.laelops.model.Usuario;
+import com.higor.laelops.repository.UsuarioRepository;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
     public Usuario localizarOuCriarUsuarioPorEmail(String email) {
-        return null;
+        return usuarioRepository.findByEmail(email).orElseGet(() -> usuarioRepository.save(
+                Usuario.builder()
+                        .email(email)
+                        .emailVerificado(false)
+                        .build()
+        ));
     }
 }
+
+
